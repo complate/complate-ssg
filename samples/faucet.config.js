37e75f7914dfe766286cdf6e5185791fdc167754
FND <fnd@localhost.localdomain>
2018-02-25 11:59:11 +0100

    upgraded dependencies

--- samples/faucet.config.js
+++ samples/faucet.config.js
@@ -1,17 +1,10 @@
-let config = {
-	manifest: false,
-	bundles: [{
-		entryPoint: "./views/index.jsx",
+module.exports = {
+	js: [{
+		source: "./views/index.jsx",
 		target: "./dist/bundle.js",
 		format: "cjs",
 		moduleName: "render",
-		transpiler: {
-			features: ["es2015", "jsx"],
-			jsx: { pragma: "createElement" }
-		}
+		esnext: true,
+		jsx: { pragma: "createElement" }
 	}]
-};
-
-module.exports = {
-	js: config
 };
