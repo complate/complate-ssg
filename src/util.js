d5038b45195117ac3eea991fbf8d0d3b81cfd9b2
FND <fnd@localhost.localdomain>
2018-04-13 13:36:06 +0200

    tweaked promisify utility
    based on similar tweaks within faucet-pipeline:
    https://github.com/faucet-pipeline/faucet-pipeline/commit/3f292895784a02ee3afa646f3e50823a24408697

--- src/util.js
+++ src/util.js
@@ -5,19 +5,18 @@ exports.abort = (msg, code = 1) => {
 	process.exit(code);
 };
 
-// poor man's version of Node's `util.promisify` (available since v8.0.0)
+// simplistic imitation of Node 8's `util.promisify`
+// NB: only supports a single callback parameter
 exports.promisify = fn => {
-	return (...args) => {
-		return new Promise((resolve, reject) => {
-			fn(...args, (err, res) => {
-				if(err) {
-					reject(err);
-					return;
-				}
-				resolve(res);
-			});
+	return (...args) => new Promise((resolve, reject) => {
+		fn(...args, (err, res) => {
+			if(err) {
+				reject(err);
+				return;
+			}
+			resolve(res);
 		});
-	};
+	});
 };
 
 exports.repr = value => `\`${value}\``;
